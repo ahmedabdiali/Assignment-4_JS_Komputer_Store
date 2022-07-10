98861392f3704c89de027c3da451a06cf9bfd217
Ahmed <ahmed.abdi.ali@se.experis.com>
2022-07-10 15:24:56 +0200

    Accessing dom elements

---
+++ app.js
@@ -0,0 +1,11 @@
+const drinksElement = document.getElementById("drinks");
+const priceElement = document.getElementById("price");
+const addElement = document.getElementById("add");
+const cartElement = document.getElementById("cart");
+const quantityElement = document.getElementById("quantity");
+const payButtonElement = document.getElementById("pay");
+const totalDueElement = document.getElementById("totalDue");
+
+let drinks = [];
+let cart = [];
+let totalDue = 0.0;
