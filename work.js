1043f28e72b1f899ec84518a4f387941564363fd
Ahmed <ahmed.abdi.ali@se.experis.com>
2022-07-11 12:15:27 +0200

    added so that i can earn 100sek and push it to the bank

--- work.js
+++ work.js
@@ -3,5 +3,37 @@ const workValueElement = document.getElementById("work")
 const bankButtonElement = document.getElementById("bankButton")
 const bankValueElement = document.getElementById("bank")
 
+let earningValue = 0;
 let workValue = 0;
-let bankValue = 0;
+
+const handleWorkChange = e =>{
+    const electedComputer = computers[e.taret.selectedIndex]
+    workValueElement.innerText= electedComputer.work;
+}
+const handleBankChange = e =>{
+    const electedComputer = computers[e.taret.selectedIndex]
+    bankValueElement.innerText= electedComputer.bank;
+}
+
+////////////////////////////////////////////////////7
+
+const handleWorkLogic = e =>{
+    const money = 100;
+    workValue += parseFloat(money);
+    workValueElement.innerText = (workValue)+'sek'
+    // workValueElement.innerText="hello"
+}
+
+
+const handleBankLogic = e =>{
+   let bankValue = earningValue +=  workValue;
+   workValue=0;
+   
+   bankValueElement.innerText=(bankValue) +'sek'
+   workValueElement.innerText=(workValue) +'sek'
+// bankValueElement.innerText="hello"
+}
+
+
+workButtonElement.addEventListener('click',handleWorkLogic)
+bankButtonElement.addEventListener('click',handleBankLogic)
